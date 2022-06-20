e5c3a243206c9492ec144988cc998a6f336c8795
Suha Siddiqui <suhasid@my.yorku.ca>
2022-06-20 14:13:41 -0400

    check box example functional

---
+++ vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+  return {
+    tutorials: [],
+  }
+},
+}
+</script>
+<template>
+  <center>
+    <input
+      v-model="tutorials"
+      type="checkbox"
+      value="Data Structures"
+      name="ds"/>
+    <label for="ds">Data Structures</label><br />
+    <input v-model="tutorials" 
+           type="checkbox" 
+           value="Algorithms" 
+           name="al" />
+    <label for="al">Algorithms</label><br />
+    <input
+      v-model="tutorials"
+      type="checkbox"
+      value="Machine Learning"
+      name="ml"/>
+    <label for="ml">Machine Learning</label>
+    <p>Tutorials: {{ tutorials }}</p>
+  </center>
+</template>
